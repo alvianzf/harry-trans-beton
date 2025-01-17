069702612b0d268f8d3c8cd8b5c2eff5cef42b2a
alvianzf <alvianzf@gmail.com>
2025-01-17 13:20:00 +0700

    add commitment skeleton page

---
+++ src/components/home/Commitment.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Commitment() {
+  return (
+    <div className='our-commitment'>
+        <div className='commitment-content'>
+            <i className='fas fa-handshake big-icon'></i> <h2>Our Commitment</h2>
+            </div>
+    </div>
+  )
+}
+
+export default Commitment
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import Commitment from "../components/home/Commitment"
 import Hero from "../components/home/Hero"
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Commitment />
     </div>
   )
 }
