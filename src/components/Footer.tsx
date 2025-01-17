ea4a8cd3627c863eae7520b4aee7e4381bfc41ab
alvianzf <alvianzf@gmail.com>
2025-01-17 18:50:34 +0700

    feat: add plant information to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,7 @@ function Footer() {
           <img className="footer-logo" src={logo} alt="logo" />
           <h2>PT. HARRY TRANS BETON</h2>
         </div>
+        <h3>Contact Information</h3>
         <p>
           <i className="fas fa-map-marker-alt info"></i> Jl. Lintas Duri - Dumai
           Km.11 Air Kulim - Bathin Solapan - Bengkalis
@@ -18,6 +19,22 @@ function Footer() {
         <p>
           <i className="fas fa-phone info"></i>{" "}
           <strong>0812170459803/082172260888</strong>
+        </p>
+        <h3>Our Plant</h3>
+        <p>
+          <i className="fas fa-industry info"></i> Wet Mix System Plant
+        </p>
+        <p>
+          Para ahli mencampurkan seluruh bahan material, semen, additive, dan
+          air ke dalam wadah mixing untuk diolah secara bersamaan.
+        </p>
+        <p style={{ marginTop: "1rem"}}>
+          <i className="fas fa-truck info"></i> Mobile Plant
+        </p>
+        <p>
+          Dikenal juga sebagai pabrik beton protable yang sangat handal karena
+          dapat dioperasikan di tempat atau lokasi proyek dan dapat
+          berpindah-pindah dari satu proyek ke proyek lainnya.
         </p>
       </div>
       <div className="footer-content">
