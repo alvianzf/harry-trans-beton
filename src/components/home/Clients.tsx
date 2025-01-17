2b3b20686041ec60a89f73872fa0aac3762aabdf
alvianzf <alvianzf@gmail.com>
2025-01-17 17:36:34 +0700

    feat: clients section

--- src/components/home/Clients.tsx
+++ src/components/home/Clients.tsx
@@ -1,7 +1,28 @@
+import pertamina from '../../assets/images/clients/pertamina.png'
+import pertaminaRokan from '../../assets/images/clients/peramina-rokan.png'
+import halliburton from '../../assets/images/clients/halliburton.png'
+import adhiKarya from '../../assets/images/clients/adhi-karya.png'
+import nindya from '../../assets/images/clients/nindya.png'
+import hki from '../../assets/images/clients/HKI.png'
+import transSumatera from '../../assets/images/clients/trans-sumatera.png'
+import imk from '../../assets/images/clients/imk.png'
+import pas from '../../assets/images/clients/pas.png'
+
 
 function Clients() {
   return (
-    <div>Clients</div>
+    <div className="clients">
+        <h2>Our Clients</h2>
+        <img src={pertamina} alt="Pertamina" />
+        <img src={pertaminaRokan} alt="Pertamina Rokan" />
+        <img src={halliburton} alt="Halliburton" />
+        <img src={adhiKarya} alt="Adhi Karya" />
+        <img src={nindya} alt="Nindya" />
+        <img src={hki} alt="HKI" />
+        <img src={transSumatera} alt="Trans Sumatera" />
+        <img src={imk} alt="IMK" />
+        <img src={pas} alt="PAS" />
+    </div>
   )
 }
 
