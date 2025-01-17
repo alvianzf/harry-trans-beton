e451bd55b1c32aa8a921320a706ec732188b9861
alvianzf <alvianzf@gmail.com>
2025-01-17 16:08:49 +0700

    feat: navbar always stay on top

--- src/components/home/Commitment.tsx
+++ src/components/home/Commitment.tsx
@@ -1,11 +1,24 @@
 function Commitment() {
   return (
-    <div className='our-commitment'>
-        <div className='commitment-content'>
-            <i className='fas fa-handshake big-icon'></i> <h2>Our Commitment</h2>
-            </div>
+    <div className="our-commitment" >
+      <div className="commitment-content" style={{ textAlign: "center" }}>
+        <i className="fas fa-handshake big-icon"></i>
+        <h2>Commitment</h2>
+      </div>
+      <div className="commitment-content">
+        <p>
+          We are proud to announce that Perkasa Beton Batam (PBB) has received
+          the certification to ISO 9001:2015 by TUV Rheinland. ISO 9001 is the
+          standard for ‘quality management’ across our entire business. The
+          certification will support us to maintain the highest levels of
+          service quality that today’s business environment demands. Our clients
+          now have complete assurance that Perkasa Beton Batam (PBB) operates to
+          the highest quality standards, while we will continually strive to
+          maintain and improve these standards that set us apart.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Commitment
+export default Commitment;
