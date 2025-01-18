693e974ae788ac3bd606f1a95222cbe26ad79661
alvianzf <alvianzf@gmail.com>
2025-01-18 19:48:37 +0700

    fix: modify whatsapp button no text behaviour

--- src/components/navigations/Hamburger.tsx
+++ src/components/navigations/Hamburger.tsx
@@ -1,28 +1,17 @@
-import React from 'react';
-
-const Hamburger = () => {
-  const [menuOpen, setMenuOpen] = React.useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  return (
-    <div className="hamburger">
-        <div className="menu-icon" onClick={toggleMenu}>
-          <i className="fa fa-bars"></i>
-        </div>
-      {menuOpen && (
-        <div className="menu">
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Hamburger;
+function WhatsappButton({ text }: { text: boolean }) {
+    return (
+      <div className="whatsapp-link">
+        <a
+          href="https://wa.me/62812170459803"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-whatsapp"></i>
+          {text && "WhatsApp"}
+        </a>
+      </div>
+    );
+  }
+  
+  export default WhatsappButton;
+  
